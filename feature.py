3a2ef422a629fe8d3a5a8394adebe22acef38066
dodecaphobia <cba30@scarletmail.rutgers.edu>
2018-05-06 11:05:56 -0400

    feature cleaned and tested

--- feature.py
+++ feature.py
@@ -1,23 +1,27 @@
 import os
 
-#reads inputs as text files, with labels in a separate text file as a single line
-#returns single matrix, formatted for training, each contained array holding <classification, feature 1, feature 2, etc.>
-def getFeaturesJoined(baseFilePath, prefix, imageList):
-	#read label file and store in list
-	categories = []
-	labelPath = os.path.join(baseFilePath, "labels.txt")
-	with open(labelPath, "r") as f:
-		for line in f:
-			for i in len(line):
-				categories.append(int(line[i]))
+#modified from Berkeley's provided code, retrieves all labels from a file
+def getLabels(baseFilePath, percent):
+	labelPath = os.path.join(baseFilePath, "labels")
+	labelsFull = []
+	if(os.path.exists(labelPath)): 
+		fin = [l[:-1] for l in open(labelPath).readlines()]
+		labelsFull = []
+		for line in fin:
+			if line == '':
+				break
+			labelsFull.append(int(line))
 
+	return labelsFull
+
+#retrieves data from images with indices taken from imageList, converted to features
+def getFeatures(baseFilePath, prefix, imageList):
 	#read image files and generate features, append to matrix
 	featureMatrix = []
 	for i in range(len(imageList)):
 		imagePath = prefix + str(imageList[i]) + ".txt"
 		featurePath = os.path.join(baseFilePath, imagePath)
 		featureList = []
-		featureList.append(categories[i])
 		with open(featurePath, "r") as f:
 			for line in f:
 				for c in line:
@@ -27,56 +31,3 @@ def getFeaturesJoined(baseFilePath, prefix, imageList):
 						featureList.append(1)
 		featureMatrix.append(featureList)
 	return featureMatrix
-
-#same as above, but separated feature and labels lists
-def getFeaturesSeparate():
-	#read label file and store in list
-	categories = []
-	labelPath = os.path.join(baseFilePath, "labels.txt")
-	with open(labelPath, "r") as f:
-		for line in f:
-			for i in len(line):
-				categories.append(int(line[i]))
-
-	#read image files and generate features, append to matrix
-	featureMatrix = []
-	for i in range(len(imageList)):
-		imagePath = prefix + str(imageList[i]) + ".txt"
-		featurePath = os.path.join(baseFilePath, imagePath)
-		featureList = []
-		with open(featurePath, "r") as f:
-			for line in f:
-				for c in line:
-					if c == ' ':
-						featureList.append(0)
-					else:
-						featureList.append(1)
-		featureMatrix.append(featureList)
-	return categories, featureMatrix
-
-#same as above, but covers all in section, suitable for testing
-#first output is list of labels, second is matrix of features
-def getFeaturesTest(baseFilePath, prefix, lenElements):
-	#read label file and store in list
-	categories = []
-	labelPath = os.path.join(baseFilePath, "labels.txt")
-	with open(labelPath, "r") as f:
-		for line in f:
-			for i in len(line):
-				categories.append(int(line[i]))
-
-	#read image files and generate features, append to matrix
-	featureMatrix = []
-	for i in range(lenElements):
-		imagePath = prefix + i + ".txt"
-		featurePath = os.path.join(baseFilePath, imagePath)
-		featureList = []
-		with open(featurePath, "r") as f:
-			for line in f:
-				for c in line:
-					if c == ' ':
-						featureList.append(0)
-					else:
-						featureList.append(1)
-		featureMatrix.append(featureList)
-	return categories, featureMatrix
